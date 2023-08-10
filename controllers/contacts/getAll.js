1022d0444c0b7d4d7ad3c188005ef6fe0fada1f3
Inna Nomerovkina <inkosof@gmail.com>
2023-08-11 02:56:07 +0300

    Update getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,10 +1,11 @@
+import { query } from "express";
 import Contact from "../../models/contacts.js";
 
 const getAll = async (req, res) => {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10, ...query } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({owner},"-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
+    const result = await Contact.find({owner, ...query},"-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
     res.json(result);
 };
 
